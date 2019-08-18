7c1ae8cc6763a9f36dd910fb669c34f2d49062ae
Pierre <38128948+PierreKieffer@users.noreply.github.com>
2019-08-18 21:15:47 +0200

    Add files via upload

--- src/main/scala/GenericMachineLearningAlg.scala
+++ src/main/scala/GenericMachineLearningAlg.scala
@@ -1,3 +1,7 @@
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.{SparkConf, SparkContext}
+
+
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.RandomForestClassifier
@@ -8,26 +12,26 @@ import org.apache.spark.sql.types.StringType
 
 import scala.collection.mutable
 
-object GenericMachineLearningAlg {
+object GenericMachineLearningAlg extends SparkSessionBase {
 
-  def ConvertEx(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("sparkConfDataScienceAPPTest")
-    val sc = new SparkContext(sparkConf)
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  def machineLearningEx(): Unit = {
 
-    val spark = org.apache.spark.sql.SparkSession.builder
-      .appName("sparkSessionDataScienceAPP")
-      .getOrCreate
-    spark.conf.set("spark.sql.session.timeZone", "GMT")
-    spark.sparkContext.setLogLevel("ERROR")
 
-    // Import dataframe
-    val path = args(0)
-    val Database = spark.read.format("csv").option("header","true").option("inferschema","true").load(path)
+    /** Importer les données */
+    val path = ""
+    val Database = sparkSession.read.format("csv").option("header","true").option("inferschema","true").load(path)
 
-    // Separate Numeric and no Numeric features
-    val NoNumFeaturesColNames = mutable.ArrayBuffer[String]()
-    val NumFeaturesColNames = mutable.ArrayBuffer[String]()
+    println("----------------------------")
+    println("DATA")
+    println("----------------------------")
+
+    Database.show(8000)
+
+
+
+    /** Lister les colonnes de features en fonction du type => Séparer les colonnes "numérique" et "non numériques" */
+    val NoNumFeaturesColNames = mutable.ArrayBuffer[String]() // Noms de colonnes non numériques
+    val NumFeaturesColNames = mutable.ArrayBuffer[String]() // Noms de colonnes numériques
     Database.schema.map(col => {
       if(col.dataType == StringType){
         NoNumFeaturesColNames += (col.name.toString)
@@ -36,26 +40,36 @@ object GenericMachineLearningAlg {
       }
     })
 
-    // Label
-    val labelColName = args(1) // "Class"
+
+
+    /** Creation de noms de colonnes :
+      * Nom de colonne initiale = ColName
+      * Nom de colonne après préprocessing = ColName_Indexed*/
+
+
+
+    /** Colonne des labels */
+    val labelColName = ""
     val idxdLabelColName = labelColName + "Indexed"
 
-    // Features
+    /** colonnes features*/
     val idxdNoNumFeaturesColNames = NoNumFeaturesColNames.map(_ + "Indexed")
     val allFeaturesColNamesInterm = NumFeaturesColNames ++ NoNumFeaturesColNames
     val allIdxdFeaturesColNamesIntermediate = NumFeaturesColNames ++ idxdNoNumFeaturesColNames
     val features = "Features"
+
     val allFeaturesColNames = allFeaturesColNamesInterm.filter(_ != labelColName)
     val allIdxdFeaturesColNames = allIdxdFeaturesColNamesIntermediate.filter(_ != idxdLabelColName)
 
-    // Train Test split
-    val splits = Database.randomSplit(Array(0.7,0.3))
+    /** Train test split */
+    val splits = Database.randomSplit(Array(0.9,0.1))
     val (trainingData, testData) = (splits(0),splits(1))
     val allTrainingData = trainingData.select(labelColName, allFeaturesColNames: _*)
     val allTestData = testData.select(labelColName, allFeaturesColNames: _*)
     val allData = allTrainingData.union(allTestData)
 
-    //Encode from string to int Features
+
+    /** Encoder de String vers Int les features*/
     val stringIndexers = NoNumFeaturesColNames.map{colName =>
       new StringIndexer()
         .setInputCol(colName)
@@ -63,56 +77,59 @@ object GenericMachineLearningAlg {
         .fit(allData)
     }
 
-    // Encode from string to int Labels
+    /** Encoder de String vers Int les labels*/
     val labelIndexer = new StringIndexer()
       .setInputCol(labelColName)
       .setOutputCol(idxdLabelColName)
       .fit(allData)
 
-    // Vector assembler
+    /** Vectoriser les features*/
     val assembler = new VectorAssembler()
       .setInputCols(Array(allIdxdFeaturesColNames: _*))
       .setOutputCol(features)
 
-    // Random Forest
+    /** Random forest */
     val randomForest = new RandomForestClassifier()
       .setLabelCol(idxdLabelColName)
       .setFeaturesCol(features)
 
-    // Label Converter 
+    /** Reconvertir les labels Int vers String */
     val labelConverter = new IndexToString()
       .setInputCol("prediction")
       .setOutputCol("predictionLabel")
       .setLabels(labelIndexer.labels)
 
-    // Pipeline
+    /** Définir la pipeline */
     val pipeline = new Pipeline().setStages(
       (stringIndexers  :+ assembler :+ randomForest :+ labelConverter).toArray)
 
-    // Cross validation parameters
+    /** Paramêtres de validation croisée */
     val paramGrid = new ParamGridBuilder()
       .addGrid(randomForest.maxBins, Array(25,28,31))
       .addGrid(randomForest.maxDepth, Array(4,6,8))
       .addGrid(randomForest.impurity, Array("entropy","gini"))
       .build()
 
-    // Evaluator
     val evaluator = new BinaryClassificationEvaluator()
       .setLabelCol(idxdLabelColName)
 
-    // Cross validator
     val cv = new CrossValidator()
       .setEstimator(pipeline)
       .setEvaluator(evaluator)
       .setEstimatorParamMaps(paramGrid)
       .setNumFolds(3)
 
-    // Train model on train set
+    /** Entrainement du model */
     val crossValidatorModel = cv.fit(allTrainingData)
 
-    // Prediction on test set
+
+    println("----------------------------")
+    println("PREDICTION")
+    println("----------------------------")
+
+    /** Prediction sur set de test */
     val predictions = crossValidatorModel.transform(allTestData)
-    predictions.show()
+    predictions.show(5000)
 
   }
 }
